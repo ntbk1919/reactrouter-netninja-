4b40fd5598b66df8d3e648f8ab23389cbf27009b
ntbk1919 <noeltbejoy@gmail.com>
2020-06-06 07:43:15 +0530

    lesson 29

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
 import React from 'react'
 const Contact=(props)=>{
-    console.log(props)
+ setTimeout(()=>{
+     props.history.push('/about')
+ },2000)
     return(
         <div className="container">
             <h4 className="center">Contact</h4>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import {Link,NavLink} from 'react-router-dom'
-const Navbar=()=>{
+import {Link,NavLink,withRouter} from 'react-router-dom'
+const Navbar=(props)=>{
+    setTimeout(()=>{
+        props.history.push('/about')
+    },2000)
     return (
     <nav className="nav-wrapper red darken-3">
     <div className="container">
@@ -14,4 +17,4 @@ const Navbar=()=>{
     </nav>
     )
 }
-export default Navbar;
+export default withRouter( Navbar);
